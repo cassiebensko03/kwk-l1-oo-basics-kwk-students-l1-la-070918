7ec00eb83c63aea86b1f7e751ba194f0ce6ca7d7
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-16 19:01:00 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -27,3 +27,16 @@
 # end 
 
 # Book.new("And Then There Were None")
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
