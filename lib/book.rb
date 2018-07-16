149e3aadef57df2d7a604add05c9fddba4b641f0
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-16 19:00:54 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,29 +1,29 @@
-class Book
-end
-attr_accessor :title, :author, :page_count, :genre, :turn_page
-  def initialize(title, author, page_count, genre, turn_page)
-    @title = title
-  @author = author 
-end
+# class Book
+# end
+# attr_accessor :title, :author, :page_count, :genre, :turn_page
+#   def initialize(title, author, page_count, genre, turn_page)
+#     @title = title
+#   @author = author 
+# end
 
-def page_count = (num)
-  @page_count = num
-end
+# def page_count = (num)
+#   @page_count = num
+# end
 
-def page_count
-  @page_count = page_count
+# def page_count
+#   @page_count = page_count
   
-def genre=(genre)
-    @genre = genre
-  end
+# def genre=(genre)
+#     @genre = genre
+#   end
 
-  def genre
-    @genre
-  end
+#   def genre
+#     @genre
+#   end
 
-def turn_page
-      puts "Flipping the page...wow, this is a damn good book!"
+# def turn_page
+#       puts "Flipping the page...wow, this is a damn good book!"
 
-end 
+# end 
 
-Book.new("And Then There Were None")
+# Book.new("And Then There Were None")
